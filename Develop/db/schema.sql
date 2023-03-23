b212a8b787cf595cd65d727039b982d7b4e503af
fardinatabassum <fardinatabassum@gmail.com>
2023-03-23 16:14:22 -0400

    deleted schema and created models

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,28 +4,3 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
-use ecommerce_db; 
-
-CREATE TABLE Category (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    category_name STRING NOT NULL
-);
-
-CREATE TABLE Product (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    product_name STRING NOT NULL,
-    Price DEC NOT NULL,
-    stock INT NOT NULL,
-    category_id REFERENCES Category(id)
-);
-
-CREATE TABLE Tag (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    tag_name STRING
-);
-
-CREATE TABLE ProductTag (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    product_id INT REFERENCES Product(id),
-    tag_id INT REFERENCES Tag(id)
-);
