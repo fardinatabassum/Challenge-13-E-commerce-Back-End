592df627e4696fafd70c4879ab8af68f7301799e
fardinatabassum <fardinatabassum@gmail.com>
2023-03-26 22:01:19 -0400

    added commenting on all files

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,9 +1,13 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// import our database connection from config.js
 const sequelize = require('../config/connection');
 
+// Initialize Product Tag model (table) by extending off Sequelize's Model class
 class ProductTag extends Model {}
 
+// Sets up fields and rules for Product Tag model
 ProductTag.init(
   {
     id: {
